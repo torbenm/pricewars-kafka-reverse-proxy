d5d47d2f85d0fcbd5957ced6bfcf5e84ef5e9c0e
jaSunny <github@sunny.remote.mx>
2016-11-29 11:33:23 +0100

    adding additional topic form kafka

--- LoggerApp.py
+++ LoggerApp.py
@@ -31,5 +31,25 @@ def getAll():
     consumer.close()
     return(json.dumps(result))
 
+@app.route("/log/buyOffer")
+def getAll():
+    kafka_endpoint = 'vm-mpws2016hp1-05.eaalab.hpi.uni-potsdam.de'
+    consumer = KafkaConsumer(consumer_timeout_ms = 3000, bootstrap_servers = kafka_endpoint + ':9092')
+
+    consumer.assign([TopicPartition('buyOffer', 0)])
+    consumer.seek_to_beginning()
+
+    result = []
+
+    for msg in consumer:
+        try:
+            msg2 = json.loads(msg.value.decode('utf-8'))
+            result.append({"topic": msg.topic,"timestamp": msg.timestamp,"value": msg2})
+        except:
+            pass
+
+    consumer.close()
+    return(json.dumps(result))
+
 if __name__ == "__main__":
     app.run(port=8001)
